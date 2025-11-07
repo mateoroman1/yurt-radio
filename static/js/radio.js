5cb8c3df380ca7f89d026cc95e9777f11fdee1cb
mateoroman1 <128441281+mateoroman1@users.noreply.github.com>
2025-11-07 14:52:51 -0600

    fixed progress bar seeking

--- static/js/radio.js
+++ static/js/radio.js
@@ -10,6 +10,7 @@
 
 // ==================== CONFIGURATION ====================
 
+// UPDATE FOR PROD const API_BASE_URL = '/api';
 const API_BASE_URL = 'http://127.0.0.1:5000/api';
 
 // ==================== STATE ====================
