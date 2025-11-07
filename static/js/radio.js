10152221d03a3491c62ab035dae4851a5ec2109e
mateoroman1 <128441281+mateoroman1@users.noreply.github.com>
2025-11-07 15:16:36 -0600

    update base api

--- static/js/radio.js
+++ static/js/radio.js
@@ -11,7 +11,7 @@
 // ==================== CONFIGURATION ====================
 
 // UPDATE FOR PROD const API_BASE_URL = '/api';
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = '/api';
 
 // ==================== STATE ====================
 
